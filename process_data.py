329901f11d1064bb1b635a43db668569fd33e3dc
CrisCobos96 <cristhiancm96@gmail.com>
2025-01-25 21:16:26 -0500

    Creación proyecto vacío.

---
+++ process_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np 
+
+# archivo para procesamiento, lectura y filtrado de dataframes
---
+++ process_image.py
@@ -0,0 +1,7 @@
+from process_data import *
+import pandas as pd
+import plotly.express as px
+import dash
+
+
+# archivo pensado para generar imágenes
